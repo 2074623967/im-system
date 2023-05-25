c91ee2f6c61ae5e740879aa03d95a392e41cadce
tangchungjiao <2074623967@qq.com>
2023-05-25 12:26:42 +0800

    初始化项目搭建

---
+++ im-service/src/main/java/com/lld/im/service/ServiceApplication.java
@@ -0,0 +1,15 @@
+package com.lld.im.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tangcj
+ * @date 2023/05/24 22:08
+ **/
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
